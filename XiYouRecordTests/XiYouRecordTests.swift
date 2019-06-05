cae61ad579c0192837ed6ecfdc89ce49288c7437
LifeiyanWhy <1553652636@qq.com>
2019-06-05 09:43:17 +0800

    Initial Commit

---
+++ XiYouRecordTests/XiYouRecordTests.swift
@@ -0,0 +1,34 @@
+//
+//  XiYouRecordTests.swift
+//  XiYouRecordTests
+//
+//  Created by 李飞艳 on 2019/6/5.
+//  Copyright © 2019年 李飞艳. All rights reserved.
+//
+
+import XCTest
+@testable import XiYouRecord
+
+class XiYouRecordTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
